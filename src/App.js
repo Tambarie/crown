f4d1f997c821710d27b2d77a3dea5e23e7fcfffd
tambarie <gt.gbaragbo@gmail.com>
2020-08-27 21:45:14 +0100

    restructured the components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Homepage from '../src/components/homepage-components'
+import Homepage from './Pages/homepage/homepage-components'
 
 import './App.css';
 
---
+++ src/Pages/homepage/homepage-components.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Directory from '../../components/directory/directory.component';
+import './homepage-component-styles.scss';
+
+
+const Homepage = () => {
+    return (
+        <main className='homepage'>
+            <Directory />
+
+        </main>
+    );
+}
+
+
+export default Homepage;
