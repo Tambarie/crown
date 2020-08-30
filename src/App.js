624b55b67e8d50331ea74f4b68a1b78ea69735f4
tambarie <gt.gbaragbo@gmail.com>
2020-08-30 09:27:19 +0100

    Added the shop-page component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
 import Homepage from './Pages/homepage/homepage-components'
-
+import ShopPage from './Pages/shopPage/shopPage-Component'
 import './App.css';
+
 
 function App() {
   return (
     <div>
-      <Homepage></Homepage>
+      <Switch>
+      <Route exact path='/' component={Homepage} />
+      <Route path='/shop' component={ShopPage} />
+
+      </Switch>
+
     </div>
+
+
   );
 }
 
